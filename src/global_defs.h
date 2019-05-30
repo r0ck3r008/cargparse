34555746c8792ba5d53f171aeb822ae1a667c49d
r0ck3r008 <naman.arora@tutanota.com>
2019-05-30 19:05:17 +0530

    added a name attribute to struct arg

--- src/global_defs.h
+++ src/global_defs.h
@@ -6,6 +6,7 @@ struct arg
 	int req;
 	char *s_name;	//5
 	char *l_name;	//15
+	char *name;	//15
 	char *help_msg; //50
 	char *value;	//10
 	struct arg *nxt, *prev;
@@ -13,8 +14,9 @@ struct arg
 
 struct arg *init_lib();
 void show_help(struct arg *);
-void add_argument(struct arg *, char *, char *, char *, int);
+void add_argument(struct arg *, char *, char *, char *, char *, int);
 void parse_args(struct arg *, int, char **);
+char *find_arg_val(struct arg *, char *);
 void clean(struct arg *);
 
 #endif
